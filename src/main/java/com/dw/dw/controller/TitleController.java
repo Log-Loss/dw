c5e58274af75ee75e731ececfd2a27fcc66eb5dd
qyh <imqyh@qq.com>
2017-12-28 15:02:31 +0800

    add paging

--- src/main/java/com/dw/dw/controller/TitleController.java
+++ src/main/java/com/dw/dw/controller/TitleController.java
@@ -14,32 +14,32 @@ public class TitleController {
     private TitleRepository titleRepository;
 
     @RequestMapping(value = "/title/{title}", method = RequestMethod.GET)
-    public Object index(@PathVariable String title) {
+    public Object index(@PathVariable String title, Integer page, Integer pageSize) {
         Object result = titleRepository.findByTitle(title);
         return new Response(200, "OK", result, false);
     }
 
     @RequestMapping(value = "/title/containing/{title}", method = RequestMethod.GET)
-    public Object containing(@PathVariable String title) {
+    public Object containing(@PathVariable String title, Integer page, Integer pageSize) {
         Object result = titleRepository.findByTitleContaining(title);
-        return new Response(200, "OK", result, false);
+        return new Response(200, "OK", result, true, page, pageSize);
     }
 
     @RequestMapping(value = "/title/like/{title}", method = RequestMethod.GET)
-    public Object like(@PathVariable String title) {
+    public Object like(@PathVariable String title, Integer page, Integer pageSize) {
         Object result = titleRepository.findByTitleLike(title);
-        return new Response(200, "OK", result, false);
+        return new Response(200, "OK", result, true, page, pageSize);
     }
 
     @RequestMapping(value = "/title/starting/{title}", method = RequestMethod.GET)
-    public Object starting(@PathVariable String title) {
+    public Object starting(@PathVariable String title, Integer page, Integer pageSize) {
         Object result = titleRepository.findByTitleStartingWith(title);
-        return new Response(200, "OK", result, false);
+        return new Response(200, "OK", result, true, page, pageSize);
     }
 
     @RequestMapping(value = "/title/contains/{title}", method = RequestMethod.GET)
-    public Object contains(@PathVariable String title) {
+    public Object contains(@PathVariable String title, Integer page, Integer pageSize) {
         Object result = titleRepository.findByTitleContains(title);
-        return new Response(200, "OK", result, false);
+        return new Response(200, "OK", result, true, page, pageSize);
     }
 }
